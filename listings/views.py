d6d797d4761ec427b8f508c826b91de3b6b776f3
khalil saidane <khalilsaidane@khalils-MacBook-Pro.local>
2019-12-17 16:15:05 +0100

    search field and search page

--- listings/views.py
+++ listings/views.py
@@ -24,4 +24,40 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains= keywords)
+    # City
+    if 'cirty' in request.GET:
+        # City is the name of the field
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+    # State
+    if 'state' in request.GET:
+        # state is the name of the field
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        # bedrooms is the name of the field
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            # Less then
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+    # Price
+    if 'price' in request.GET:
+        # bedrooms is the name of the field
+        price = request.GET['price']
+        if price:
+            # Less then
+            query_list = query_list.filter(price__lte=price)
+    context = {
+        'listings': query_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
